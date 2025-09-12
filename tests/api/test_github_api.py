fe8cd988bbadc8b3bd7f7a4f2f4ee62e2abc0dbf
Galyna Svynarenko <svinarenko@gmail.com>
2025-09-12 17:21:06 +0300

    tests for search remote repo are added

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -11,3 +11,20 @@ def test_user_exists(github_api):
 def test_non_exist_user(github_api):
     r = github_api.get_user('butenko_sergii')
     assert r['message'] == 'Not Found'
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    # print(r)
+    assert r['total_count'] == 57
+    assert 'become-qa-auto' in r['items'][0]['name']
+    
+@pytest.mark.api
+def test_cannot_be_fount(github_api):
+    r = github_api.search_repo('gs_repo_non_exist')
+    assert r['total_count'] == 0
+
+@pytest.mark.api
+def test_one_symbol_name(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
